edc1f01f7841fd0828b943b41f41ace13b5aad27
Your Name <you@example.com>
2023-06-13 21:08:31 +0800

    drop-keys

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -4,5 +4,5 @@ batch_dir=data/hunyuan7b_generations/
     --batch_dir ${batch_dir} \
     --num_instructions_to_generate 50000 \
     --seed_tasks_path data/seed_tasks.jsonl \
-    --api_key 'hCKimvqJWfK16svqcgtGxP1/P4M5wv0a' \
+    --api_key 'xxx/P4M5wv0a' \
     --engine "sec-x-text-hunyuan-7b-001"
